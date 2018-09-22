66a115e170ef527cfb4f9c2152086a4699d07764
TobKed <tobiaszkedzierski@gmail.com>
2018-09-22 21:06:59 +0200

    add __str__ methods for models register models in admin

--- src/contacts/models.py
+++ src/contacts/models.py
@@ -5,12 +5,18 @@ class ContactGroup(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField(null=True)
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Address(models.Model):
     city = models.CharField(max_length=64)
     street = models.CharField(max_length=64, null=True)
     building_number = models.IntegerField(null=True)
     flat_number = models.IntegerField(null=True)
+
+    def __str__(self):
+        return f"{self.city} {self.street} {self.building_number} {self.flat_number}"
 
 
 class Person(models.Model):
@@ -20,14 +26,23 @@ class Person(models.Model):
     address = models.ForeignKey(Address, null=True, on_delete=models.SET_NULL)
     groups = models.ManyToManyField(ContactGroup)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class PhoneNumber(models.Model):
     number = models.IntegerField()
     type = models.CharField(max_length=64)
     owner = models.ForeignKey(Person, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.number}"
+
 
 class Email(models.Model):
     address = models.CharField(max_length=128)
     type = models.CharField(max_length=64)
     owner = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.address}"
