e849eae0298263f10d836d99727e76440a9a8f71
TobKed <tobiaszkedzierski@gmail.com>
2018-09-22 14:51:39 +0200

    register contacts app add Person and Address models

--- src/contacts/models.py
+++ src/contacts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    city = models.CharField(max_length=64)
+    street = models.CharField(max_length=64, null=True)
+    building_number = models.IntegerField(null=True)
+    flat_number = models.IntegerField(null=True)
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    description = models.TextField(null=True)
+    address = models.ForeignKey(Address, null=True, on_delete=models.SET_NULL)
