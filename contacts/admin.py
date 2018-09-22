67478767ee4f0564be369edcb787fa7a80ac8393
TobKed <tobiaszkedzierski@gmail.com>
2018-09-22 21:06:59 +0200

    add __str__ methods for models register models in admin

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Person, Address, PhoneNumber, Email, ContactGroup
 
-# Register your models here.
+admin.site.register(Person)
+admin.site.register(Address)
+admin.site.register(PhoneNumber)
+admin.site.register(Email)
+admin.site.register(ContactGroup)
