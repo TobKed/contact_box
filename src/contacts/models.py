fc6690a6ffbfb1a372b097b897c4deaffc863b0b
TobKed <tobiaszkedzierski@gmail.com>
2018-09-22 15:12:08 +0200

    add ContactGroup, PhoneNumber, Email model

--- src/contacts/models.py
+++ src/contacts/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 
+class ContactGroup(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField(null=True)
+
+
 class Address(models.Model):
     city = models.CharField(max_length=64)
     street = models.CharField(max_length=64, null=True)
@@ -13,3 +18,16 @@ class Person(models.Model):
     last_name = models.CharField(max_length=64)
     description = models.TextField(null=True)
     address = models.ForeignKey(Address, null=True, on_delete=models.SET_NULL)
+    groups = models.ManyToManyField(ContactGroup)
+
+
+class PhoneNumber(models.Model):
+    number = models.IntegerField()
+    type = models.CharField(max_length=64)
+    owner = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+
+class Email(models.Model):
+    address = models.CharField(max_length=128)
+    type = models.CharField(max_length=64)
+    owner = models.ForeignKey(Person, on_delete=models.CASCADE)
