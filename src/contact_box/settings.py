976d28a889cf7d450d84300d12d6a0e891102c0c
TobKed <tobiaszkedzierski@gmail.com>
2018-09-26 20:30:51 +0200

    add users app

--- src/contact_box/settings.py
+++ src/contact_box/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'contacts.apps.ContactsConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
