8fe312ece705a444ca4ec906033e3d0ca578ca5f
TobKed <tobiaszkedzierski@gmail.com>
2018-09-30 02:22:21 +0200

    add requirements.txt add django-extensions app

--- src/contact_box/settings.py
+++ src/contact_box/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_extensions',
     'contacts.apps.ContactsConfig',
     'users.apps.UsersConfig',
     'django.contrib.admin',
