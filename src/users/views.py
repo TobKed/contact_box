f64b32d4eb413cd930b3755109b9caac0b02cd00
TobKed <tobiaszkedzierski@gmail.com>
2018-09-30 12:59:32 +0200

    fix register view, add logout view

--- src/users/views.py
+++ src/users/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 
 def register(request):
-    form = UserCreationForm()
+    form = UserRegisterForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
+
     return render(request, 'users/register.html', {'form': form})
