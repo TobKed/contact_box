3c624dcb9549ed0ae0c4e9619c436509d38a020a
TobKed <tobiaszkedzierski@gmail.com>
2018-09-30 19:49:28 +0200

    add contacts view

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import UserContactsListView
 
 
 urlpatterns = [
     path('', views.home,  name='contacts-home'),
+    path('contacts/', UserContactsListView.as_view(),  name='contacts-user-contacts'),
 ]
