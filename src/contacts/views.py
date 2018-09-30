16f8011bc297e9e6f476558791b9e81dae3d40b9
TobKed <tobiaszkedzierski@gmail.com>
2018-09-30 19:49:28 +0200

    add contacts view

--- src/contacts/views.py
+++ src/contacts/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+from django.contrib.auth.models import User
+from .models import Person
 
 
 def home(request):
     return render(request, 'contacts/base.html')
+
+
+class UserContactsListView(ListView):
+    model = Person
+    template_name = 'contacts/home.html'
+    context_object_name = 'persons'
+    paginate_by = 5
+
+    def get_queryset(self):
+        print('TEST', self.request.user)
+        user = get_object_or_404(User, username=self.request.user)
+        return Person.objects.filter(creator=user.profile).order_by('last_name')
