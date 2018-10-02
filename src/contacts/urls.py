ba5ff21f317c1797c306e3e71c285459b26a4c8c
TobKed <tobiaszkedzierski@gmail.com>
2018-10-02 20:17:31 +0200

    add person detail view

--- src/contacts/urls.py
+++ src/contacts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import UserContactsListView
+from .views import UserContactsListView, PersonDetailView
 
 
 urlpatterns = [
     path('', views.home,  name='contacts-home'),
-    path('contacts/', UserContactsListView.as_view(),  name='contacts-user-contacts'),
+    path('contacts/', UserContactsListView.as_view(),  name='contacts'),
+    path('person/<int:pk>/', PersonDetailView.as_view(), name='person-detail')
 ]
--- src/contacts/views.py
+++ src/contacts/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+    ListView,
+    DetailView
+)
 from .models import Person
 
 
@@ -11,10 +14,14 @@ def home(request):
 
 class UserContactsListView(LoginRequiredMixin, ListView):
     model = Person
-    template_name = 'contacts/user-contacts.html'
+    template_name = 'contacts/contacts.html'
     context_object_name = 'persons'
     paginate_by = 5
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.request.user)
         return Person.objects.filter(creator=user.profile).order_by('last_name')
+
+
+class PersonDetailView(DetailView):
+    model = Person
