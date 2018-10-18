26a59ab45b880acf82c2c5a11a31aee50bdb4866
TobKed <tobiaszkedzierski@gmail.com>
2018-10-18 07:06:26 +0200

    update person update view (inline formsets)

--- contacts/forms.py
+++ contacts/forms.py
@@ -3,3 +3,6 @@ from .models import Person, PhoneNumber
 
 
 PhoneFormSet = inlineformset_factory(Person, PhoneNumber, fields=['number', 'type'], extra=1)
+
+def PhoneFormSetFunc(extra=1, *args, **kwargs):
+    return inlineformset_factory(Person, PhoneNumber, fields=['number', 'type'], extra=extra, *args, **kwargs)
