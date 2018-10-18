d02570b9b972247b2e02e88baf2ec395021a9c56
TobKed <tobiaszkedzierski@gmail.com>
2018-10-18 06:05:25 +0200

    update inline formset for person-create view

--- src/contacts/views.py
+++ src/contacts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
@@ -49,12 +50,42 @@ class PersonCreateView(LoginRequiredMixin, CreateView):
         form_class = self.get_form_class()
         form = self.get_form(form_class)
         phone_form = PhoneFormSet()
-        return self.render_to_response(self.get_context_data(form=form, phone_form=phone_form))
+        return self.render_to_response(
+            self.get_context_data(
+                    form=form,
+                    phone_form=phone_form))
 
-    def form_valid(self, form):
+    def post(self, request, *args, **kwargs):
+        """
+        Handles POST requests, instantiating a form instance and its inline
+        formsets with the passed POST variables and then checking them for
+        validity.
+        """
+        self.object = None
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        phone_form = PhoneFormSet(self.request.POST)
+        if form.is_valid() and phone_form.is_valid():
+            return self.form_valid(form, phone_form)
+        else:
+            return self.form_invalid(form, phone_form)
+
+    def form_valid(self, form, phone_form):
         self.object = form.save(commit=False)
         self.object.creator_id = self.request.user.pk
-        return super().form_valid(form)
+        self.object = form.save()
+        phone_form.instance = self.object
+        phone_form.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+    def form_invalid(self, form, phone_form,):
+        """
+        Called if a form is invalid. Re-renders the context data with the
+        data-filled forms and errors.
+        """
+        return self.render_to_response(
+            self.get_context_data(form=form,
+                                  phone_form=phone_form))
 
 
 class PersonUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
