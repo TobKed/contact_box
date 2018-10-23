07d428dcb8bebbcaeca6e52dcaa92efc22ae3590
TobKed <tobiaszkedzierski@gmail.com>
2018-10-23 20:54:05 +0200

    refactor

--- src/contacts/forms.py
+++ src/contacts/forms.py
@@ -2,5 +2,5 @@ from django.forms.models import inlineformset_factory
 from .models import Person, PhoneNumber, Email
 
 
-PhoneFormSet = inlineformset_factory(Person, PhoneNumber, fields=['number', 'type'], extra=1)
-EmailFormSet = inlineformset_factory(Person, Email, fields=['address', 'type'], extra=1)
+PhoneFormSet = inlineformset_factory(Person, PhoneNumber, fields='__all__', extra=1)
+EmailFormSet = inlineformset_factory(Person, Email, fields='__all__', extra=1)
