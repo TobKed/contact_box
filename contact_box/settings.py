be6d3b8f63c1f45be3ff0e5b39985b171768e4e4
TobKed <tobiaszkedzierski@gmail.com>
2018-11-03 13:43:35 +0100

    updates for heroku

--- contact_box/settings.py
+++ contact_box/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -150,3 +150,6 @@ LOGIN_URL = 'login'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 INTERNAL_IPS = ['127.0.0.1']
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
