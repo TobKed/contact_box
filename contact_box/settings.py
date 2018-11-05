468719266354b9564a956c0566258f00953c9968
TobKed <tobiaszkedzierski@gmail.com>
2018-11-05 22:57:13 +0100

    fix for heroku

--- contact_box/settings.py
+++ contact_box/settings.py
@@ -52,10 +52,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    # Simplified static file serving.
-    # https://warehouse.python.org/project/whitenoise/
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
