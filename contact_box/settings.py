3c5e1ef4c129ebae05eda21aae3144e6fc65d71d
TobKed <tobiaszkedzierski@gmail.com>
2018-11-05 22:54:13 +0100

    fix whitenoise for heroku

--- contact_box/settings.py
+++ contact_box/settings.py
@@ -146,9 +146,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
