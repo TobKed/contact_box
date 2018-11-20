4d76f3f4a5c29d06e0ac13668990c9b0d9439cea
TobKed <tobiaszkedzierski@gmail.com>
2018-11-20 21:05:22 +0100

    update settings for static files

--- src/contact_box/settings.py
+++ src/contact_box/settings.py
@@ -147,6 +147,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'contacts-home'
